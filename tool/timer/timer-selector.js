5ad6f47ef55cd8655fe778fc893241a047160e7b
Puton1221 <88196889+Puton1221@users.noreply.github.com>
2022-05-18 11:45:26 +0900

    Update timer-selector.js

--- tool/timer/timer-selector.js
+++ tool/timer/timer-selector.js
@@ -505,7 +505,7 @@ function setup() {
 setTimeout(setup, 5);
 
   //コピーボタンの実装
-function copy() {
+function copy {
   var copyTarget = document.getElementById("output");
   copyTarget.select();
   document.execCommand("Copy");
